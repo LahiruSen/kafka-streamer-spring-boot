9056379483c25fd81e1d2e57da3cffa5d9653279
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-09-18 00:05:02 +1000

    first commit

---
+++ src/main/java/com/lahiru/kafkastreamerspringboot/KafkaStreamerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lahiru.kafkastreamerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lahiru/kafkastreamerspringboot/KafkaStreamerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lahiru.kafkastreamerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
