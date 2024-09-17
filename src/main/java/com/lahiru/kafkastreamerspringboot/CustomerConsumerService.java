698258e2eb8110d6e3ef58b58e444376c6d43179
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-09-18 02:01:51 +1000

    added business logic to customer consumer service.

--- src/main/java/com/lahiru/kafkastreamerspringboot/CustomerConsumerService.java
+++ src/main/java/com/lahiru/kafkastreamerspringboot/CustomerConsumerService.java
@@ -1,14 +1,39 @@
 package com.lahiru.kafkastreamerspringboot;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerConsumerService {
 
+    @Autowired
+    private CustomerProducerService customerProducerService;
+
     @KafkaListener(topics = "input-topic", groupId = "my-group")
     public void consume(String message) {
         System.out.println("Received message: " + message);
+
+        int age = extractAgeFromMessage(message);
+
+        // Determine if age is even or odd and publish to the appropriate topic
+        if (age % 2 == 0) {
+            customerProducerService.sendMessage("CustomerEVEN", message);
+        } else {
+            customerProducerService.sendMessage("CustomerODD", message);
+        }
+    }
+
+    private int extractAgeFromMessage(String message) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(message);
+            return jsonNode.get("age").asInt();
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing message: " + message, e);
+        }
     }
 
 
